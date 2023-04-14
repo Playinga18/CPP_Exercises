dca5bea482ad2d422f0a3b8b921786a54d9e0bf6
Playinga18 <91030442+Playinga18@users.noreply.github.com>
2023-04-14 16:33:20 +0200

    tp8 - j'aime pas ce tp

--- src/Kitchen.cpp
+++ src/Kitchen.cpp
@@ -1,1 +1,12 @@
 #include "Kitchen.hpp"
+#include <utility>
+
+const Unit& Kitchen::register_unit(const Unit& unit) {
+    auto [it, emplaced] = _units.emplace(std::move(unit));
+    return *it;
+}
+
+const Unit* Kitchen::find_unit(const string& name) const{
+    const auto& it = _units.find(name, [](const auto& n, const auto& u) { return ;});
+    return it != _units.end() ? &it->second : nullptr;    
+}
--- src/Kitchen.hpp
+++ src/Kitchen.hpp
@@ -1,4 +1,23 @@
 #pragma once
 
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <set>
+
+#include "../lib/Unit.hpp"
+#include "../lib/Ingredient.hpp"
+
+using namespace std;
+
 class Kitchen
-{};
+{
+    public:
+        Kitchen() = default;
+        const Unit& register_unit(const Unit& unit);
+        const Unit* find_unit(const string& name) const;
+
+    private:  
+        set<Unit> _units;
+        set<Ingredient> _ingredients;
+};
